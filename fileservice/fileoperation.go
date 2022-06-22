e96702ad091c9ce9c28f1ebfc97760a101054792
AllenIversons <hanbaofu95@163.com>
2022-06-22 15:48:44 +0800

    update

--- fileservice/fileoperation.go
+++ fileservice/fileoperation.go
@@ -2,43 +2,82 @@ package fileservice
 
 import (
 	"context"
+	"fmt"
+	"github.com/spf13/viper"
 	"github.com/tencentyun/cos-go-sdk-v5"
 	"io"
 	"net/http"
 	"net/url"
+	"time"
 )
 
-
-type FileOperation struct {
-	Url 		*url.URL
-	SecretID 	string
-	SecretKey	string
+const EXPIRETIME = time.Hour
+type CosConfig struct {
+	URL 		string		`mapstructure:"URL"`
+	SecretID 	string		`mapstructure:"Secret_ID"`
+	SecretKey	string		`mapstructure:"Secret_Key"`
 }
 
-func NewFileOperation (url *url.URL,secretID,secretKey string,) *FileOperation {
-	return &FileOperation{
-		url,
-		secretID,
-		secretKey,
-	}
-}
+var COSCONFIG CosConfig
 
-func (fOperation *FileOperation) Upload(key string,content io.Reader) (*cos.Response,error){
-
-	b := &cos.BaseURL{BucketURL: fOperation.Url}
-	c := cos.NewClient(b, &http.Client{
+func GetCosClient() *cos.Client{
+	url, _ := url.Parse(COSCONFIG.URL)
+	b := &cos.BaseURL{BucketURL: url}
+	client := cos.NewClient(b, &http.Client{
 		Transport: &cos.AuthorizationTransport{
-			SecretID:  fOperation.SecretID,
-			SecretKey: fOperation.SecretKey,
+			SecretID:  COSCONFIG.SecretID,
+			SecretKey: COSCONFIG.SecretKey,
 		},
 	})
+	return client
+}
 
-
-	resp, err := c.Object.Put(context.Background(), key,content,nil)
+func Upload(key string,content io.Reader) (*cos.Response,error){
+	client := GetCosClient()
+	resp, err := client.Object.Put(context.Background(), key,content,nil)
 	if err != nil {
 		return resp,err
 	}
+
 	return resp,nil
 }
+
+func AccessFileObject(preSignedUrl *url.URL) {
+	resp, err := http.Get(preSignedUrl.String())
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+	}
+	defer resp.Body.Close()
+	fmt.Println(preSignedUrl.String())
+	fmt.Printf("resp:%v\n", resp)
+}
+
+func GeneratePreSignedUrl(key string) (*url.URL,error){
+	client := GetCosClient()
+	presignedURL, err := client.Object.GetPresignedURL(context.Background(),http.MethodGet,key,COSCONFIG.SecretID,COSCONFIG.SecretKey,EXPIRETIME,nil)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(presignedURL)
+	return presignedURL,err
+}
+
+func LoadConfig(path string,config *CosConfig) error {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+
+	viper.AutomaticEnv()
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		return err
+	}
+	err = viper.Unmarshal(config)
+	return err
+}
+
+
 
 
--- fileservice/fileoperation_test.go
+++ fileservice/fileoperation_test.go
@@ -1,80 +1,48 @@
 package fileservice
 
 import (
-	"context"
 	"fmt"
-	"github.com/tencentyun/cos-go-sdk-v5"
+	"io/ioutil"
 	"net/http"
-	"net/url"
 	"strings"
 	"testing"
 )
 
-func TestCtreatBucket(t *testing.T){
-	// 将 examplebucket-1250000000 和 COS_REGION 修改为真实的信息
-	// 存储桶名称，由bucketname-appid 组成，appid必须填入，可以在COS控制台查看存储桶名称。https://console.cloud.tencent.com/cos5/bucket
-	// COS_REGION 可以在控制台查看，https://console.cloud.tencent.com/cos5/bucket, 关于地域的详情见 https://cloud.tencent.com/document/product/436/6224
-	u, _ := url.Parse("https://unifit-1311571861.cos.ap-guangzhou.myqcloud.com")
-	b := &cos.BaseURL{BucketURL: u}
-	c := cos.NewClient(b, &http.Client{
-		Transport: &cos.AuthorizationTransport{
-			SecretID:  "AKIDRikVzB8oDKBm68tOcYDcka9RSDhurYx5",   // 替换为用户的 SecretId，请登录访问管理控制台进行查看和管理，https://console.cloud.tencent.com/cam/capi
-			SecretKey: "tVqtgDUYLtqWw71ez4sPedMp42kBLErD",  // 替换为用户的 SecretKey，请登录访问管理控制台进行查看和管理，https://console.cloud.tencent.com/cam/capi
-		},
-	})
-
-
-	_, err := c.Bucket.Put(context.Background(), nil)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func TestGetBucket(t *testing.T) {
-
-	// 将 examplebucket-1250000000 和 COS_REGION 修改为真实的信息
-	// 存储桶名称，由bucketname-appid 组成，appid必须填入，可以在COS控制台查看存储桶名称。https://console.cloud.tencent.com/cos5/bucket
-	// COS_REGION 可以在控制台查看，https://console.cloud.tencent.com/cos5/bucket, 关于地域的详情见 https://cloud.tencent.com/document/product/436/6224
-	c := cos.NewClient(nil, &http.Client{
-		Transport: &cos.AuthorizationTransport{
-			SecretID:  "AKIDRikVzB8oDKBm68tOcYDcka9RSDhurYx5",   // 替换为用户的 SecretId，请登录访问管理控制台进行查看和管理，https://console.cloud.tencent.com/cam/capi
-			SecretKey: "tVqtgDUYLtqWw71ez4sPedMp42kBLErD",  // 替换为用户的 SecretKey，请登录访问管理控制台进行查看和管理，https://console.cloud.tencent.com/cam/capi
-		},
-	})
-
-
-	s, _, err := c.Service.Get(context.Background())
-	if err != nil {
-		panic(err)
-	}
-
-	for _, b := range s.Buckets {
-		fmt.Printf("%#v\n", b)
-	}
-
-}
-
 func TestUpload (t *testing.T)  {
 
-	u, _ := url.Parse("https://unifit-1311571861.cos.ap-guangzhou.myqcloud.com")
-	b := &cos.BaseURL{BucketURL: u}
-	c := cos.NewClient(b, &http.Client{
-		Transport: &cos.AuthorizationTransport{
-			SecretID:  "AKIDRikVzB8oDKBm68tOcYDcka9RSDhurYx5",
-			SecretKey: "tVqtgDUYLtqWw71ez4sPedMp42kBLErD",
-		},
-	})
-	// 对象键（Key）是对象在存储桶中的唯一标识。
-	// 例如，在对象的访问域名 `examplebucket-1250000000.cos.COS_REGION.myqcloud.com/test/objectPut.go` 中，对象键为 test/objectPut.go
-	name := "test1/objectPut.go"
-	// 1.通过字符串上传对象
-	f := strings.NewReader("test")
-
-
-	resp, err := c.Object.Put(context.Background(), name, f, nil)
-	if err != nil {
-		panic(err)
+	err := LoadConfig("../config",&COSCONFIG)
+	if err !=nil{
+		fmt.Println(err)
 	}
+	fmt.Println(COSCONFIG)
+	name := "unifit/文件名"
 
+	f := strings.NewReader("test")
+	resp,err := Upload(name,f)
+	//resp, err := client.Object.Put(context.Background(), name, f, nil)
+	if err != nil {
+	}
 	fmt.Printf("%s\n", resp.Status)
 }
+
+
+func TestGeneratePreSignedUrl (t *testing.T)  {
+	//var cosConfig CosConfig
+	err := LoadConfig("../config",&COSCONFIG)
+	if err !=nil{
+		fmt.Println(err)
+	}
+	name := "unifit/文件名"
+	presignedURL,err := GeneratePreSignedUrl(name)
+	if err != nil {
+		fmt.Println(err)
+	}
+	resp2, err := http.Get(presignedURL.String())
+	if err != nil {
+		fmt.Println(err)
+	}
+	bs2, _ := ioutil.ReadAll(resp2.Body)
+	fmt.Println(string(bs2))
+	resp2.Body.Close()
+}
+
