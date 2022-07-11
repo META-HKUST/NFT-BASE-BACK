664dc511f71c1e42eed0eb5396c34120665b628a
AllenIversons <hanbaofu95@163.com>
2022-07-11 16:34:41 +0800

    update

--- docs/docs.go
+++ docs/docs.go
@@ -2293,7 +2293,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8889",
+	Host:             "unifit.ust/hk:8889",
 	BasePath:         "/api/v2",
 	Schemes:          []string{},
 	Title:            "HKUST-NFT",
