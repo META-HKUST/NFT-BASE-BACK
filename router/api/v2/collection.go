8db871aa4b382ab14703b8c084db4379e05b50ad
YezzizzeY <1721062927@qq.com>
2022-07-18 23:26:45 +0800

    label input update

--- router/api/v2/collection.go
+++ router/api/v2/collection.go
@@ -13,15 +13,15 @@ import (
 )
 
 type CreateCollectionRequest struct {
-	Collection_name string `json:"collection_name" example:"Pixel Pear" default:"Pixel Pear"`
-	LogoImage       string `json:"logo_image" default:"https://img-ae.seadn.io/https%3A%2F%2Flh3.googleusercontent.com%2Fsvc_rQkHVGf3aMI14v3pN-ZTI7uDRwN-QayvixX-nHSMZBgb1L1LReSg1-rXj4gNLJgAB0-yD8ERoT-Q2Gu4cy5AuSg-RdHF9bOxFDw%3Ds10000?fit=max&h=2500&w=2500&auto=format&s=61a1f05fd1f4a891c9b8fc197befc0a9"`
-	LogoImage_S     string `json:"log_image_signature" default:"ABCDE"`
-	FeatureImage    string `json:"feature_image" default:"https://img-ae.seadn.io/https%3A%2F%2Flh3.googleusercontent.com%2Fsvc_rQkHVGf3aMI14v3pN-ZTI7uDRwN-QayvixX-nHSMZBgb1L1LReSg1-rXj4gNLJgAB0-yD8ERoT-Q2Gu4cy5AuSg-RdHF9bOxFDw%3Ds10000?fit=max&h=2500&w=2500&auto=format&s=61a1f05fd1f4a891c9b8fc197befc0a9"`
-	FeatureImagee_S string `json:"feature_image_signature" default:"ABCDE"`
-	BannerImage     string `json:"banner_image" default:"https://img-ae.seadn.io/https%3A%2F%2Flh3.googleusercontent.com%2Fsvc_rQkHVGf3aMI14v3pN-ZTI7uDRwN-QayvixX-nHSMZBgb1L1LReSg1-rXj4gNLJgAB0-yD8ERoT-Q2Gu4cy5AuSg-RdHF9bOxFDw%3Ds10000?fit=max&h=2500&w=2500&auto=format&s=61a1f05fd1f4a891c9b8fc197befc0a9P"`
-	BannerImage_S   string `json:"banner_image_signature" default:"ABCDE"`
-	Description     string `json:"description" example:"Happy Happy Happy Happy" default:"Happy Happy Happy Happy"`
-	Label           string `json:"label" example:"Pear&Pixel&Wechat" default:"Pear&Pixel&Wechat"`
+	Collection_name string   `json:"collection_name" example:"Pixel Pear" default:"Pixel Pear"`
+	LogoImage       string   `json:"logo_image" default:"https://img-ae.seadn.io/https%3A%2F%2Flh3.googleusercontent.com%2Fsvc_rQkHVGf3aMI14v3pN-ZTI7uDRwN-QayvixX-nHSMZBgb1L1LReSg1-rXj4gNLJgAB0-yD8ERoT-Q2Gu4cy5AuSg-RdHF9bOxFDw%3Ds10000?fit=max&h=2500&w=2500&auto=format&s=61a1f05fd1f4a891c9b8fc197befc0a9"`
+	LogoImage_S     string   `json:"log_image_signature" default:"ABCDE"`
+	FeatureImage    string   `json:"feature_image" default:"https://img-ae.seadn.io/https%3A%2F%2Flh3.googleusercontent.com%2Fsvc_rQkHVGf3aMI14v3pN-ZTI7uDRwN-QayvixX-nHSMZBgb1L1LReSg1-rXj4gNLJgAB0-yD8ERoT-Q2Gu4cy5AuSg-RdHF9bOxFDw%3Ds10000?fit=max&h=2500&w=2500&auto=format&s=61a1f05fd1f4a891c9b8fc197befc0a9"`
+	FeatureImagee_S string   `json:"feature_image_signature" default:"ABCDE"`
+	BannerImage     string   `json:"banner_image" default:"https://img-ae.seadn.io/https%3A%2F%2Flh3.googleusercontent.com%2Fsvc_rQkHVGf3aMI14v3pN-ZTI7uDRwN-QayvixX-nHSMZBgb1L1LReSg1-rXj4gNLJgAB0-yD8ERoT-Q2Gu4cy5AuSg-RdHF9bOxFDw%3Ds10000?fit=max&h=2500&w=2500&auto=format&s=61a1f05fd1f4a891c9b8fc197befc0a9P"`
+	BannerImage_S   string   `json:"banner_image_signature" default:"ABCDE"`
+	Description     string   `json:"description" example:"Happy Happy Happy Happy" default:"Happy Happy Happy Happy"`
+	Label           []string `json:"label" example:"Music,Comics"`
 }
 
 // Create @Description  create: 创建一个collection
@@ -59,16 +59,16 @@ func Create(ctx *gin.Context) {
 }
 
 type EditCollectionRequest struct {
-	Collection_id   int    `json:"collection_id" example:"111111" default:"111111"`
-	Collection_name string `json:"collection_name" example:"Pixel Pear" default:"Pixel Pear"`
-	LogoImage       string `json:"logo_image" default:"https://img-ae.seadn.io/https%3A%2F%2Flh3.googleusercontent.com%2Fsvc_rQkHVGf3aMI14v3pN-ZTI7uDRwN-QayvixX-nHSMZBgb1L1LReSg1-rXj4gNLJgAB0-yD8ERoT-Q2Gu4cy5AuSg-RdHF9bOxFDw%3Ds10000?fit=max&h=2500&w=2500&auto=format&s=61a1f05fd1f4a891c9b8fc197befc0a9"`
-	LogoImage_S     string `json:"log_image_signature" default:"ABCDE"`
-	FeatureImage    string `json:"feature_image" default:"https://img-ae.seadn.io/https%3A%2F%2Flh3.googleusercontent.com%2Fsvc_rQkHVGf3aMI14v3pN-ZTI7uDRwN-QayvixX-nHSMZBgb1L1LReSg1-rXj4gNLJgAB0-yD8ERoT-Q2Gu4cy5AuSg-RdHF9bOxFDw%3Ds10000?fit=max&h=2500&w=2500&auto=format&s=61a1f05fd1f4a891c9b8fc197befc0a9"`
-	FeatureImagee_S string `json:"feature_image_signature" default:"ABCDE"`
-	BannerImage     string `json:"banner_image" default:"https://img-ae.seadn.io/https%3A%2F%2Flh3.googleusercontent.com%2Fsvc_rQkHVGf3aMI14v3pN-ZTI7uDRwN-QayvixX-nHSMZBgb1L1LReSg1-rXj4gNLJgAB0-yD8ERoT-Q2Gu4cy5AuSg-RdHF9bOxFDw%3Ds10000?fit=max&h=2500&w=2500&auto=format&s=61a1f05fd1f4a891c9b8fc197befc0a9P"`
-	BannerImage_S   string `json:"banner_image_signature" default:"ABCDE"`
-	Description     string `json:"description" example:"Happy Happy Happy Happy" default:"Happy Happy Happy Happy"`
-	Label           string `json:"label" example:"Pear,Pixel,Wechat" default:"Pear&Pixel&Wechat"`
+	Collection_id   int      `json:"collection_id" example:"111111" default:"111111"`
+	Collection_name string   `json:"collection_name" example:"Pixel Pear" default:"Pixel Pear"`
+	LogoImage       string   `json:"logo_image" default:"https://img-ae.seadn.io/https%3A%2F%2Flh3.googleusercontent.com%2Fsvc_rQkHVGf3aMI14v3pN-ZTI7uDRwN-QayvixX-nHSMZBgb1L1LReSg1-rXj4gNLJgAB0-yD8ERoT-Q2Gu4cy5AuSg-RdHF9bOxFDw%3Ds10000?fit=max&h=2500&w=2500&auto=format&s=61a1f05fd1f4a891c9b8fc197befc0a9"`
+	LogoImage_S     string   `json:"log_image_signature" default:"ABCDE"`
+	FeatureImage    string   `json:"feature_image" default:"https://img-ae.seadn.io/https%3A%2F%2Flh3.googleusercontent.com%2Fsvc_rQkHVGf3aMI14v3pN-ZTI7uDRwN-QayvixX-nHSMZBgb1L1LReSg1-rXj4gNLJgAB0-yD8ERoT-Q2Gu4cy5AuSg-RdHF9bOxFDw%3Ds10000?fit=max&h=2500&w=2500&auto=format&s=61a1f05fd1f4a891c9b8fc197befc0a9"`
+	FeatureImagee_S string   `json:"feature_image_signature" default:"ABCDE"`
+	BannerImage     string   `json:"banner_image" default:"https://img-ae.seadn.io/https%3A%2F%2Flh3.googleusercontent.com%2Fsvc_rQkHVGf3aMI14v3pN-ZTI7uDRwN-QayvixX-nHSMZBgb1L1LReSg1-rXj4gNLJgAB0-yD8ERoT-Q2Gu4cy5AuSg-RdHF9bOxFDw%3Ds10000?fit=max&h=2500&w=2500&auto=format&s=61a1f05fd1f4a891c9b8fc197befc0a9P"`
+	BannerImage_S   string   `json:"banner_image_signature" default:"ABCDE"`
+	Description     string   `json:"description" example:"Happy Happy Happy Happy" default:"Happy Happy Happy Happy"`
+	Label           []string `json:"label" example:"Music,Comics"`
 }
 
 // Edit  @Description  edit : 输入邮箱、验证码和密码，重新设置已经忘记的密码
