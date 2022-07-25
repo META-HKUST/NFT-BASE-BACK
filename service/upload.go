06d7e9a6f00bec7f7e8f212ce29a3b54d4d2bbeb
AllenIversons <hanbaofu95@163.com>
2022-07-25 21:42:48 +0800

    update

--- service/upload.go
+++ service/upload.go
@@ -3,19 +3,18 @@ package service
 import (
 	"NFT-BASE-BACK/fileservice"
 	"NFT-BASE-BACK/utils"
-	"bytes"
 	"io"
 	"log"
 )
 
 func Upload(key string,content io.Reader) (string,string,error){
-	img,err := utils.CompressImg(content,key,500)
-	if err != nil {
-		log.Println(err)
-		return "","",err
-	}
-	readers := bytes.NewReader(img)
-	_, url, err := fileservice.Upload(key,readers)
+	//img,err := utils.CompressImg(content,key,500)
+	//if err != nil {
+	//	log.Println(err)
+	//	return "","",err
+	//}
+	//readers := bytes.NewReader(img)
+	_, url, err := fileservice.Upload(key,content)
 	if err != nil {
 		log.Println(err)
 		return "","",err
